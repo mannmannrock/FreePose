45d755cec685c5acfdbb9325df2edc70908cb157
mannmannrock <mannsytha@gmail.com>
2025-02-05 13:39:00 -0500

    Initial Commit

---
+++ FreePose/FreePoseApp.swift
@@ -0,0 +1,33 @@
+//
+//  FreePoseApp.swift
+//  FreePose
+//
+//  Created by Mann Sy Tha on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FreePoseApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
---
+++ FreePoseTests/FreePoseTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreePoseTests.swift
+//  FreePoseTests
+//
+//  Created by Mann Sy Tha on 2/5/25.
+//
+
+import Testing
+@testable import FreePose_
+
+struct FreePoseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
